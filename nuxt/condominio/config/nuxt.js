d7ef719b64908953cc2a3b949f0138d04bfc76e4
Jadson Santos <jadson@info.ufrn.br>
2019-07-24 09:58:11 +0000

    nuxt project
    Signed-off-by: Jadson Santos <jadson@info.ufrn.br>

--- nuxt/condominio/config/nuxt.js
+++ nuxt/condominio/config/nuxt.js
@@ -38,12 +38,7 @@ module.exports = {
   modules: [
     // Doc: https://bootstrap-vue.js.org/docs/
     'bootstrap-vue/nuxt',
-    '@nuxtjs/axios',
   ],
-
-  axios: {
-    // proxyHeaders: false
-  },
 
   /*
   ** Build configuration
--- nuxt/condominio/resources/pages/users/index.vue
+++ nuxt/condominio/resources/pages/users/index.vue
@@ -1,4 +1,3 @@
-
 <template>
     <div class="container">
         Users Page 
@@ -8,30 +7,39 @@
                 <span>{{ user.pessoa.nome }}</span>
             </li>
         </ul>
+        
+        <input type="button" class="primary" value="Salvar Usuario" v-on:click="saveUser()"> 
+
     </div>    
 </template>
 
 <script>
     
+    import axios from 'axios'
 
     export default {
 
         data: function () {
             return {
-                users: []
+                users: [],
+                user: { "id": 3, "pessoa": { "id": 3, "nome": "Joao"} }
             }
         },
 
         methods:{
-            getUsers() {
-                const {data} = axios.get('http://localhost:8080/acervus-services/public/users/')
-                return {users:data}
+            saveUser(){
+               axios.post('http://localhost:8080/acervus-services/public/users/', this.user )
             }
         },
 
-        beforeCreate(){
-            this.getUsers()
-        }
+        //  asyncData is called every time before loading the page component. It will be called server-side once
+        // (on the first request to the Nuxt app) and client-side when navigating to further routes. 
+        async asyncData ({ params }) {
+            let { data } = await axios.get('http://localhost:8080/acervus-services/public/users/')
+            return { users: data }
+        },
+
+
     }
 </script>
 
